323fe0af461c5f9294a57b5f2014f5cb295b0514
Eli Peters <elip271828@gmail.com>
2025-11-12 18:43:41 -0500

    Set up pages Also I added a smooth transition for collapsing the head.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css'
 import Head from './Head.jsx'
+import Home from './Home.jsx'
+import Blog from './Blog.jsx'
+import About from './About.jsx'
 
 function App() {
   const [head, setHead] = useState(true)
@@ -8,6 +12,13 @@ function App() {
   return (
     <>
     <Head head={head} setHead={setHead}/>
+    <Router>
+      <Routes>
+        <Route path ="/" element={<Home/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </Router>
     </>
   )
 }
--- src/Head.jsx
+++ src/Head.jsx
@@ -1,21 +1,32 @@
 import './Head.css'
+import {useState} from "react"
 
 function Head({head, setHead}) {
+    const [headPos, setHeadPos] = useState(0)
+
+    function moveHeadUp() {
+        setTimeout(() => {setHead(prev => !prev)}, 1500)
+        setHeadPos(-12.5)
+    }
+    function moveHeadDown() {
+        setTimeout(() => {setHead(prev => !prev)}, 1500)
+        setHeadPos(0)
+    }
+
     return(
         <>
-        {head && (<>
-        <div className="head">
+        <div className="head" style={{'--head-pos': `${headPos}%`}}>
             <button className="head-button a">Home</button>
             <button className="head-button b">Blog</button>
             <button className="head-button c">About</button>
         </div>
-        <button className="close-head a" onClick={() => {setHead(prev => !prev)}}></button>
-        <button className="close-head b" onClick={() => {setHead(prev => !prev)}}></button>
+        {head && (<>
+        <button className="close-head a" onClick={() => {moveHeadUp()}}></button>
+        <button className="close-head b" onClick={() => {moveHeadUp()}}></button>
         </>)}
         {!head && (<>
-        <div className="not-head"></div>
-        <button className="close-head a" onClick={() => {setHead(prev => !prev)}}></button>
-        <button className="close-head b" onClick={() => {setHead(prev => !prev)}}></button>
+        <button className="close-head a" onClick={() => {moveHeadDown()}}></button>
+        <button className="close-head b" onClick={() => {moveHeadDown()}}></button>
         </>)}
         </>
     )
