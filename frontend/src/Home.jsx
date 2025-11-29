4365f6c58af61ecd886c13312b6537c32cbd8915
Eli Peters <elip271828@gmail.com>
2025-11-28 22:23:35 -0500

    Added stats. Tried to make it revolve smoothly, it failed misserably, had to backtrack. On a positive note, I added 2 stats, Hackatime and Chess.

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,10 +4,16 @@ import Foot from './Foot.jsx'
 function Home() {
     useEffect(() => {
         document.title="Home"
+
+        fetch('https://api.chess.com/pub/player/elithegreatest1/stats')
+        .then(res => res.json())
+        .then(data => setChessData(data))
     }, [])
 
     const [stat, setStat] = useState(0)
     const statArray = ["a", "b", "c"]
+
+    const [chessData, setChessData] = useState(null);
 
     function nextStat(sign) {
         if (sign === "+") {
@@ -47,9 +53,14 @@ function Home() {
                 <div className="flex flex-row h-50 w-[80%] bg-white text-black ml-[10%] m-10 rounded-2xl">
                     <button onClick={() => {nextStat("-")}} className="h-full left-0 w-15 bg-stone-400  text-gray-800 text-5xl opacity-50 rounded-tl-2xl rounded-bl-2xl hover:opacity-100 transition duration-250 ease">{"<"}</button>
                     <div className="flex-1 flex h-full w-160 items-center content-center">
-                        {(statArray[stat] === "a") && (<img className="w-200 h-50" src="https://github-readme-stats.hackclub.dev/api/wakatime?username=3035&api_domain=hackatime.hackclub.com&theme=prussian&custom_title=Hackatime+Stats&layout=compact&cache_seconds=0&langs_count=8"/>)}
-                        {(statArray[stat] === "b") && (<>b</>)}
-                        {(statArray[stat] === "c") && (<>c</>)}
+                        {(statArray[stat] === "a") && (<img className={`w-200 h-50`} src="https://github-readme-stats.hackclub.dev/api/wakatime?username=3035&api_domain=hackatime.hackclub.com&theme=prussian&custom_title=Hackatime+Stats&layout=compact&cache_seconds=0&langs_count=8"/>)}
+                        {(statArray[stat] === "b") && (<div className={`w-200 h-50`}>
+                            <h1 className="w-full text-4xl text-center mt-3">Chess.com</h1>
+                            <h1 className="w-full text-xl text-center mt-3">Rapid: {chessData.chess_rapid.last.rating}</h1>
+                            <h1 className="w-full text-xl text-center mt-1">Blitz: {chessData.chess_blitz.last.rating}</h1>
+                            <h1 className="w-full text-xl text-center mt-1">Bullet: {chessData.chess_bullet.last.rating}</h1>
+                        </div>)}
+                        {(statArray[stat] === "c") && (<div className={`w-200 h-50`}></div>)}
                     </div>
                     <button onClick={() => {nextStat("+")}} className="h-full right-0 w-15 bg-stone-400 text-gray-800 text-5xl opacity-50 rounded-tr-2xl rounded-br-2xl hover:opacity-100 transition duration-250 ease">{">"}</button>
                 </div>
